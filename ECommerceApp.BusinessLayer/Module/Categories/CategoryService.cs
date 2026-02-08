85174a27fe3b1798bfa5849a3cc593da36b0022b
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2026-02-08 19:59:33 +0600

    Create Business Layer for Implementing Business Logics

---
+++ ECommerceApp.BusinessLayer/Module/Categories/CategoryService.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceApp.BusinessLayer.Module.Categories.Interface;
+using ECommerceApp.Domain.Entities;
+
+namespace ECommerceApp.BusinessLayer.Module.Categories
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+            //category.CreatedDate = DateTime.Now;
+            //Category category1 = _context.Categories.FirstOrDefault(m => m.Name == category.Name);
+        }
+    }
+}
---
+++ ECommerceApp.BusinessLayer/Module/Categories/Interface/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceApp.Domain.Entities;
+
+namespace ECommerceApp.BusinessLayer.Module.Categories.Interface
+{
+    public interface ICategoryService
+    {
+        Task<bool> CreateCategoryAsync(Category category);
+    }
+}
