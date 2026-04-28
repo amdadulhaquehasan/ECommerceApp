a8f886bb88f3f0d647ceb875ebfe79492077fee6
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2026-04-28 22:39:12 +0600

    implement api based controller

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using ECommerce.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 100000 },
+            new Product { Id = 2, Name = "Mobile", Price = 20000 },
+            new Product { Id = 3, Name = "Book", Price = 1500 }
+        };
+
+        [HttpGet]
+        public List<Product> Getall()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return product;
+        }
+
+        [HttpPost]
+        public Product Create(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+            return product;
+        }
+
+        [HttpPut("{id}")]
+        public Product Update(int id, Product updateProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product is null)
+            {
+                return null;
+            }
+            product.Name = updateProduct.Name;
+            product.Price = updateProduct.Price;
+
+            return product;
+        }
+
+        [HttpPatch("{id}")]
+        public Product UpdateIndividual(int id, Product updateProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product is null)
+            {
+                return null;
+            }
+            if (!string.IsNullOrEmpty(updateProduct.Name))
+            {
+                product.Name = updateProduct.Name;
+            }
+            if (updateProduct.Price > 0)
+            {
+                product.Price = updateProduct.Price;
+            }
+            return product;
+        }
+
+        [HttpDelete("{id}")]
+        public List<Product> Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product is null)
+            {
+                return null;
+            }
+            products.Remove(product);
+            return products;
+        }
+    }
+}
--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -4,93 +4,105 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-var products = new List<Product>
-{
-    new Product { Id = 1, Name = "Laptop", Price = 100000 },
-    new Product { Id = 2, Name = "Mobile", Price = 20000 },
-    new Product { Id = 3, Name = "Book", Price = 1500 }
-};
+//var products = new List<Product>
+//{
+//    new Product { Id = 1, Name = "Laptop", Price = 100000 },
+//    new Product { Id = 2, Name = "Mobile", Price = 20000 },
+//    new Product { Id = 3, Name = "Book", Price = 1500 }
+//};
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 };
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
-// Get all products
-app.MapGet("/products", () =>
-{
-    return products;
-});
+#region Minimal API
+//// Get all products
+//app.MapGet("/products", () =>
+//{
+//    return products;
+//});
 
-// Get product by id
-app.MapGet("/products/{id}", (int id) =>
-{
-    var product = products.FirstOrDefault(p => p.Id == id);
-    return product;
-});
+//// Get product by id
+//app.MapGet("/products/{id}", (int id) =>
+//{
+//    var product = products.FirstOrDefault(p => p.Id == id);
+//    return product;
+//});
 
-// Post
-app.MapPost("/products/", (Product product) =>
-{
-    product.Id = products.Max(p => p.Id) + 1;
-    products.Add(product);
-    return product;
-});
+//// Post
+//app.MapPost("/products/", (Product product) =>
+//{
+//    product.Id = products.Max(p => p.Id) + 1;
+//    products.Add(product);
+//    return product;
+//});
 
-// put
-app.MapPut("/products/{id}", (int id, Product updateProduct) =>
-{
-    var product = products.FirstOrDefault(p => p.Id == id);
-    if (product is null)
-    {
-        return null;
-    }
-    product.Name = updateProduct.Name;
-    product.Price = updateProduct.Price;
-    return product;
-});
+//// put
+//app.MapPut("/products/{id}", (int id, Product updateProduct) =>
+//{
+//    var product = products.FirstOrDefault(p => p.Id == id);
+//    if (product is null)
+//    {
+//        return null;
+//    }
+//    product.Name = updateProduct.Name;
+//    product.Price = updateProduct.Price;
+//    return product;
+//});
 
-// patch
-app.MapPatch("/products/{id}", (int id, Product updateProduct) =>
-{
-    var product = products.FirstOrDefault(p => p.Id == id);
-    if (product is null) 
-    {
-        return null;
-    }
-    if (!string.IsNullOrEmpty(updateProduct.Name))
-    {
-        product.Name = updateProduct.Name;
-    }
-    if (updateProduct.Price > 0)
-    {
-        product.Price = updateProduct.Price;
-    }
-    return product;
-});
+//// patch
+//app.MapPatch("/products/{id}", (int id, Product updateProduct) =>
+//{
+//    var product = products.FirstOrDefault(p => p.Id == id);
+//    if (product is null) 
+//    {
+//        return null;
+//    }
+//    if (!string.IsNullOrEmpty(updateProduct.Name))
+//    {
+//        product.Name = updateProduct.Name;
+//    }
+//    if (updateProduct.Price > 0)
+//    {
+//        product.Price = updateProduct.Price;
+//    }
+//    return product;
+//});
 
-// Delete
-app.MapDelete("/products/{id}", (int id) =>
-{
-    var product = products.FirstOrDefault(p => p.Id == id);
-    if (product is null)
-    {
-        return null;
-    }
-    products.Remove(product);
-    return products;
-});
+//// Delete
+//app.MapDelete("/products/{id}", (int id) =>
+//{
+//    var product = products.FirstOrDefault(p => p.Id == id);
+//    if (product is null)
+//    {
+//        return null;
+//    }
+//    products.Remove(product);
+//    return products;
+//});
+#endregion
+
+app.MapControllers();
 
 app.Run();
 
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-}
+//public class Product
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+//    public decimal Price { get; set; }
+//}
